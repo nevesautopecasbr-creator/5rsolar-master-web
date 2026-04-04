7720a450be62ff2cf80617db13077f31979a47bb
Itallo Neves <nevesautopecasbr@gmail.com>
2026-04-04 09:47:40 -0300

    template

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Au+s4sTAbdlIxekMnD9igOpfw/IjJL6o3RMr+Ime5jQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"n73GmKLiHkaUiKUtxhaQpA6xrrd+jOC64QicrnSut8E=\"}"
